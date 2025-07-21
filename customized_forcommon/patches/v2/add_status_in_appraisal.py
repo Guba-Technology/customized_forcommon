c02ecaedd29281e07ae5557e0ee1aa2c48bce714
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2025-07-21 16:12:27 +0300

    Status field is Added in Appraisal

---
+++ customized_forcommon/patches/v2/add_status_in_appraisal.py
@@ -0,0 +1,22 @@
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
+
+def execute():
+    # Check if field already exists in meta (standard or custom)
+    appraisal = frappe.get_meta("Appraisal")
+    if "status" in [df.fieldname for df in appraisal.fields]:
+        return
+
+    # Otherwise, create the custom field
+    create_custom_fields({
+        "Appraisal": [
+            dict(
+                fieldname="status",
+                label="Status",
+                fieldtype="Select",
+                options="Pending\nApprove\nCancelled",
+                insert_after="appraisal_cycle",
+                reqd=0
+            )
+        ]
+    })
