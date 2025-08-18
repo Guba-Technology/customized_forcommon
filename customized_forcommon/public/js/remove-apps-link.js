c0365bb7a9e0f545e7ad8bd54657ca8af4a4393e
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2025-08-18 11:00:51 +0300

    Apps option is removed from the profile dropdown list

--- customized_forcommon/public/js/remove-apps-link.js
+++ customized_forcommon/public/js/remove-apps-link.js
@@ -1,17 +1,12 @@
 frappe.ready(() => {
-    const originalToolbar = frappe.ui.toolbar.Toolbar;
-
-    frappe.ui.toolbar.Toolbar = class extends originalToolbar {
-        constructor() {
-            super();
-            // Remove Apps link after header is rendered
-            const dropdown = document.querySelector('#toolbar-user');
-            if (dropdown) {
-                const appsLink = dropdown.querySelector('a[href="/apps"]');
-                if (appsLink) {
-                    appsLink.remove();
-                }
+    // Listen for dropdown opening
+    $(document).on('click', '#toolbar-user', function () {
+        const dropdown = document.querySelector('#toolbar-user');
+        if (dropdown) {
+            const appsLink = dropdown.querySelector('a[href="/apps"]');
+            if (appsLink) {
+                appsLink.remove();
             }
         }
-    };
+    });
 });
