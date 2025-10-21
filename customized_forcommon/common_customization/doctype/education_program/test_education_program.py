5eb048dd79e6ce6280426c7d7955177ab2ad5397
kelemu <kelemu3509@gmail.com>
2025-10-21 10:28:03 +0300

    first round sugar requirements added

---
+++ customized_forcommon/common_customization/doctype/education_program/test_education_program.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Guba Technology and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEducationProgram(FrappeTestCase):
+	pass
---
+++ customized_forcommon/common_customization/doctype/training_commitment/training_commitment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Guba Technology and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Training Commitment", {
+// 	refresh(frm) {
+
+// 	},
+// });
