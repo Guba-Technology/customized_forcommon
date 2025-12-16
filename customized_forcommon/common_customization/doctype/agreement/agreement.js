1dcc73a2b7a5e6c55844532f0861f8894a4c781e
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2025-12-16 09:52:22 +0300

    agreement modified

--- customized_forcommon/common_customization/doctype/agreement/agreement.js
+++ customized_forcommon/common_customization/doctype/agreement/agreement.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2025, Guba Technology and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Agreement", {
-// 	refresh(frm) {
+frappe.ui.form.on("Agreement", {
+    refresh(frm) {
 
-// 	},
-// });
+    },
+});
+
+frappe.ui.form.on('Agreement Item', {
+    qty(frm, cdt, cdn) {
+        calculate_amount(frm, cdt, cdn);
+    },
+    rate(frm, cdt, cdn) {
+        calculate_amount(frm, cdt, cdn);
+    }
+});
+
+function calculate_amount(frm, cdt, cdn) {
+    let row = frappe.get_doc(cdt, cdn);
+    row.amount = (row.qty || 0) * (row.rate || 0);
+    frappe.model.set_value(cdt, cdn, 'amount', row.amount);
+    frm.refresh_field('agreement_item');
+
+}
