0282bba85048eebb018276fc7ff770cef377fac0
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2025-12-16 11:15:17 +0300

    agreement modified

--- customized_forcommon/common_customization/doctype/agreement/agreement.py
+++ customized_forcommon/common_customization/doctype/agreement/agreement.py
@@ -1,9 +1,26 @@
 # Copyright (c) 2025, Guba Technology and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Agreement(Document):
-	pass
+    def validate(self):
+        self.validate_duplicate_items()
+
+    def validate_duplicate_items(self):
+        """Prevent duplicate item_code entries in Agreement Item child table."""
+        seen = set()
+        duplicates = []
+
+        for item in self.agreement_item:
+            if item.item_code in seen:
+                duplicates.append(item.item_code)
+            else:
+                seen.add(item.item_code)
+
+        if duplicates:
+            frappe.throw(
+                f"Duplicate items found in Agreement Items: {', '.join(set(duplicates))}"
+            )
