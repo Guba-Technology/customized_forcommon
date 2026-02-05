c1b9909fb3061f08917dd804523d872e77e2c478
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-02-05 15:58:00 +0300

    route for disalloweds

--- customized_forcommon/public/js/lite_locker.js
+++ customized_forcommon/public/js/lite_locker.js
@@ -1,16 +1,16 @@
 // your_app/public/js/lite_locker.js
-$(document).on('app_ready', function() {
+$(document).on('app_ready', function () {
     frappe.router.on('change', () => {
         const route = frappe.get_route();
         if (!route || route.length < 2) return;
 
         frappe.call({
             method: "customized_forcommon.prunning.get_locked_manifest",
-            callback: function(r) {
+            callback: function (r) {
                 if (!r.message) return;
 
                 const manifest = r.message;
-                const view_type = route[0]; 
+                const view_type = route[0];
                 const identifier = route[1];
 
                 if (view_type === 'Module' && manifest.modules.includes(identifier)) {
@@ -42,5 +42,6 @@ function block_and_redirect(item) {
         message: __("<b>{0}</b> is restricted in LITE mode Please contact your administrator.", [item]),
         indicator: 'red'
     }, 5);
-    frappe.set_route('desk');
+    // frappe.set_route('desk');
+    frappe.set_route('workspace', 'Home');
 }
