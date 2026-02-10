bfb5a8d44e3beabcaf268e2a894708a0c4752134
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-02-10 11:52:13 +0300

    leave increment period cannot be negative

--- customized_forcommon/doc_events/leave_increment_period.py
+++ customized_forcommon/doc_events/leave_increment_period.py
@@ -1,5 +1,9 @@
 import frappe
+
 def validate_leave_increment_period(doc, method):
-    leave_increment_period = frappe.db.get_value("Employee", "custom_leave_increment_period")
-    if leave_increment_period < 0:
+    # Get the leave increment period from the current Employee document
+    leave_increment_period = doc.custom_leave_increment_period
+
+    # Validate the value
+    if leave_increment_period is not None and leave_increment_period < 0:
         frappe.throw("Leave Increment Period cannot be negative")
