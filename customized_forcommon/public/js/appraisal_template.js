00c90deebb6daa89b2df90f610494fc00a3b3acc
kelemul <kelemu3509@gmail.com>
2026-02-18 14:55:52 +0300

    employee feedback criteria changed

--- customized_forcommon/public/js/appraisal_template.js
+++ customized_forcommon/public/js/appraisal_template.js
@@ -11,30 +11,30 @@ frappe.ui.form.on("Appraisal Template", {
       
     }
     ,
-    // custom_criteria_for: function(frm)
-    // {
-    //     const role_config = {
-    //    "Performance Feedback": {
-    //             show: ["rating_criteria"],
-    //             hide: ["custom_self_appraisal_rating_criteria"],
-    //         },
-    //         "Self Appraisal": {
-    //             show: ["custom_self_appraisal_rating_criteria"],
-    //             hide: ["rating_criteria"],
-    //         }
-    //     };
-    //     const config = role_config[frm.doc.custom_criteria_for];
-    //     if (config) {
+    custom_criteria_for: function(frm)
+    {
+        const role_config = {
+       "Performance Feedback": {
+                show: ["rating_criteria"],
+                hide: ["custom_self_appraisal_rating_criteria"],
+            },
+            "Self Appraisal": {
+                show: ["custom_self_appraisal_rating_criteria"],
+                hide: ["rating_criteria"],
+            }
+        };
+        const config = role_config[frm.doc.custom_criteria_for];
+        if (config) {
       
-    //         ["custom_self_appraisal_rating_criteria", "rating_criteria"].forEach(field => {
-    //             frm.set_df_property(field, "hidden", false);
+            ["custom_self_appraisal_rating_criteria", "rating_criteria"].forEach(field => {
+                frm.set_df_property(field, "hidden", false);
               
-    //         });
-    //         config.show.forEach(field => frm.set_df_property(field, "hidden", false));
-    //         config.hide.forEach(field => frm.set_df_property(field, "hidden", true));
-    //     }
+            });
+            config.show.forEach(field => frm.set_df_property(field, "hidden", false));
+            config.hide.forEach(field => frm.set_df_property(field, "hidden", true));
+        }
        
-    // }
+    }
 });
 
 
