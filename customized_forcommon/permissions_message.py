a24452d7d2cc84efaa221de1eab77db2799409c7
josyab21 <yosefabawa09@gmail.com>
2026-03-16 17:15:39 +0300

    permission removed

--- customized_forcommon/permissions_message.py
+++ customized_forcommon/permissions_message.py
@@ -1,14 +1,13 @@
 import frappe
 from frappe import _
 
-def custom_permission_message(doc, ptype, user=None, debug=False):
-    user = user or frappe.session.user
-
-    if not frappe.has_permission(doc.doctype, ptype, user=user):
-        frappe.throw(
-            _("You do not have permission to access document {1}.Please contact the administrator.").format(
-                frappe.bold(user),
-                frappe.bold(f"{doc.doctype}: {doc.name}")
-            ),
-            frappe.PermissionError
-        )
+def custom_permission_message():
+    exc = frappe.local.message_log
+    if exc:
+        for m in exc:
+            if "Not permitted" in str(m):
+                frappe.clear_messages()
+                frappe.throw(
+                    _("You do not have permission to access this document. Please contact the administrator."),
+                    frappe.PermissionError
+                )
