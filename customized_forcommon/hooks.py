8d069f8d6998788fc46fe1a353d8b48ec9b47bd2
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-03-18 14:28:29 +0300

    Daily Scheduler for Advance Repayment DOne

--- customized_forcommon/hooks.py
+++ customized_forcommon/hooks.py
@@ -201,7 +201,6 @@ scheduler_events = {
     "hourly":
     [
         "customized_forcommon.scheduler.custom_next_leave_increment_year.execute",
-        "customized_forcommon.scheduler.employee_advance.process_repayments"
     ],
 
      "daily":
