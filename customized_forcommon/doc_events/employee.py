7d68cc4f4a27d134a9103157de6afcb5945017d4
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-03-23 16:07:14 +0300

    fuel price issue fixed

--- customized_forcommon/doc_events/employee.py
+++ customized_forcommon/doc_events/employee.py
@@ -1,5 +1,5 @@
 import frappe
 def update_fuel_payment(doc, method):
     fuel_price = frappe.db.get_value("Company", doc.company, "custom_fuel_price")
-    if doc.custom_allowed_fuel > 0 and fuel_price > 0:
+    if doc.custom_allowed_fuel and doc.custom_allowed_fuel and doc.custom_allowed_fuel > 0 and fuel_price > 0:
         doc.custom_fuel_payment = doc.custom_allowed_fuel * fuel_price
