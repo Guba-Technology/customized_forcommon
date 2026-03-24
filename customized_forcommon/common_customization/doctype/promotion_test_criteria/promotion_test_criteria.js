797388b6e78110a42646d480df14104afca6eb7f
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-03-24 17:03:27 +0300

    promotion test updated

---
+++ customized_forcommon/common_customization/doctype/promotion_test_criteria/promotion_test_criteria.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Guba Technology and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Promotion Test Criteria", {
+// 	refresh(frm) {
+
+// 	},
+// });
