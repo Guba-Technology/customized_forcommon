39a807bc3460b59e1066431212a92707add27a13
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-04-28 10:54:23 +0300

    User Restriction

--- customized_forcommon/doc_events/user_access_restriction.py
+++ customized_forcommon/doc_events/user_access_restriction.py
@@ -6,33 +6,46 @@ def get_company_from_employee(user):
     return frappe.db.get_value("Employee", {"user_id": user}, "company")
 
 def user_query_condition(user):
-    """Filters the User List by joining with the Employee table."""
     if user == "Administrator":
         return ""
-    
-    company = get_company_from_employee(user)
-    
-    if company:
-        # Filter users who have an Employee record linked to the same company
-        return f"""exists (
-            select name from `tabEmployee` 
-            where `tabEmployee`.user_id = `tabUser`.name 
+
+    company = frappe.db.get_value("Employee", {"user_id": user}, "company")
+
+    if not company:
+        return "1=0"
+
+    return f"""
+        exists (
+            select 1 from `tabEmployee`
+            where `tabEmployee`.user_id = `tabUser`.name
             and `tabEmployee`.company = '{company}'
-        )"""
-    
-    # If the manager isn't an Employee or has no company, show nothing
-    return "1=0"
+        )
+    """
 
 def validate_user_access(doc, method=None):
-    """Ensures a System Manager can't open a User form from another company."""
     if frappe.session.user == "Administrator":
         return
-        
+
     my_company = get_company_from_employee(frappe.session.user)
     target_company = get_company_from_employee(doc.name)
 
     if not my_company or not target_company:
         return
-    
-    if my_company and target_company and my_company != target_company:
-        frappe.throw(_("Access Denied: This user is an employee of {0}").format(target_company), frappe.PermissionError)
+
+    if my_company != target_company:
+        frappe.throw(
+            _("Access Denied: This user is an employee of {0}").format(target_company),
+            frappe.PermissionError
+        )
+
+def has_permission(doc, ptype, user):
+    if user == "Administrator":
+        return True
+
+    my_company = frappe.db.get_value("Employee", {"user_id": user}, "company")
+    target_company = frappe.db.get_value("Employee", {"user_id": doc.name}, "company")
+
+    if not my_company or not target_company:
+        return False
+
+    return my_company == target_company
