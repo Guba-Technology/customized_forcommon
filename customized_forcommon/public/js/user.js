58de766b9851abf27b791cf19792ca4f9794ddf8
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-06-09 12:33:55 +0300

    third_party_authentication hidden

--- customized_forcommon/public/js/user.js
+++ customized_forcommon/public/js/user.js
@@ -1,5 +1,7 @@
 frappe.ui.form.on("User", {
     refresh(frm) {
         frm.set_df_property("app_section", "hidden", 1);
+        frm.set_df_property("third_party_authentication", "hidden", 1);
+
     }
 });
