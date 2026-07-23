be5d10cc3b3f5d2f1dfc1439a71ebb27469772f8
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-07-23 10:37:12 +0300

    Project Advance shortcut added in Accounting Workspace

--- customized_forcommon/common_customization/doctype/lc_master/lc_master.py
+++ customized_forcommon/common_customization/doctype/lc_master/lc_master.py
@@ -6,6 +6,24 @@ from frappe.model.document import Document
 
 
 class LCMaster(Document):
-	pass
+	def validate(self):
+		self.validate_duplicated_account()
+
+	def validate_duplicated_account(self):
+		accounts = {}
+
+		for i, row in enumerate(self.cif_value_accounts, start=1):
+			account = row.cif_value_account
+
+			if not account:
+				continue
+
+			if account in accounts:
+				frappe.throw(
+					f"CIF Value Account <b>{account}</b> is already added in row "
+					f"<b>{accounts[account]}</b>. Duplicate found in row <b>{i}</b>."
+				)
+
+			accounts[account] = i
 
 
