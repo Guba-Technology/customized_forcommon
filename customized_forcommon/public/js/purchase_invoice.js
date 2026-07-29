bd893acca48b774bda54ee74e857e58f84a919fc
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-07-29 15:26:27 +0300

    bank reconciliation reversal

--- customized_forcommon/public/js/purchase_invoice.js
+++ customized_forcommon/public/js/purchase_invoice.js
@@ -20,6 +20,8 @@ frappe.ui.form.on("Purchase Invoice", {
                 }, "Create");
             }
         }
+        customized_forcommon.setup_reverse_bank_reconciliation(frm);
+
 
     }
 });
