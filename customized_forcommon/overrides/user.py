58a26d7acad2ab40141ea815001c3ac29f7cc44d
Yeshiwas Dagnaw <58876309+Yeshiwas21@users.noreply.github.com>
2026-08-11 12:34:41 +0300

    Refactor CustomUser validation and on_update methods

--- customized_forcommon/overrides/user.py
+++ customized_forcommon/overrides/user.py
@@ -1,93 +1,43 @@
 from frappe.core.doctype.user.user import User as BaseUser
 import frappe
 
-
 class CustomUser(BaseUser):
-    EXCLUDED_USERS = ["Administrator", "Guest"]
     def validate(self):
-        # Exclude setup wizard
+        # Exclude in setup wizard
         if frappe.flags.in_setup_wizard:
             return
+        super(CustomUser, self).validate()
 
         # Exclude Administrator
         if self.name == "Administrator":
             return
 
-        # Do not disable current logged-in user
-        if frappe.session.user == self.name:
-            return
-
-        super(CustomUser, self).validate()
-
-        # Skip if this is the last active user (excluding Administrator)
-        if self._is_last_active_user():
-            return
-
-        # Only warn + disable if no company assignment exists
+       
+        # Only show warning if user has no assignment; don't forcibly disable here
         if not frappe.db.exists("User Company Assignment", {"user": self.name}):
-
-            link = (
-                f'<a href="/app/user-company-assignment/new?user={self.name}" '
-                f'style="text-decoration: underline" target="_blank">'
-                f'User Company Assignment</a>'
-            )
-
+            link = " ".join([
+                f'<a href="/app/user-company-assignment/new?user={self.name}" style="text-decoration: underline" target=_blank >User Company Assignment</a>',
+                'to assign the user to a company.'
+            ])
             self.enabled = 0
-
             frappe.msgprint(
-                f"""
-                User is not assigned to any company.<br>
-                Login will remain disabled.<br><br>
-                Add here: {link}
-                """,
-                indicator="orange"
+                f"User is not assigned to any company. Login will remain disabled. <br> Add Here {link}",
+                indicator='orange'
             )
 
     def on_update(self):
+        super(CustomUser, self).on_update()
         # Exclude Administrator
         if self.name == "Administrator":
             return
-
-        # Do not touch current logged-in user
-        if frappe.session.user == self.name:
-            return
-
-        super(CustomUser, self).on_update()
-
-        # Cleanup assignments ONLY if user is disabled
+        # Clean-up: If the user is disabled, remove all their company assignments
         if not self.enabled:
-
             assignments = frappe.get_all(
                 "User Company Assignment",
                 filters={"user": self.name},
                 pluck="name"
             )
-
-            for assignment in assignments:
-                frappe.delete_doc(
-                    "User Company Assignment",
-                    assignment,
-                    ignore_permissions=True
-                )
-
+            for assignment_name in assignments:
+                frappe.delete_doc("User Company Assignment", assignment_name, ignore_permissions=True)
             if assignments:
-                frappe.msgprint(
-                    "User has been disabled and removed from all User Company Assignments.",
-                    indicator="red"
-                )
-
-    def _is_last_active_user(self):
-        """
-        Prevent disabling the last enabled user (excluding Administrator)
-        """
-
-        active_users = frappe.db.count(
-            "User",
-            filters={
-                "enabled": 1,
-                "name": ["not in", self.EXCLUDED_USERS]
-            }
-        )
-
-        # If only one active user exists → skip disabling
-        return active_users <= 1
+                frappe.msgprint("User has been disabled and removed from all User Company Assignments.", indicator='red')
