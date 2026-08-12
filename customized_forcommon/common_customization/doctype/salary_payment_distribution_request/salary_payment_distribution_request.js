f3109ac41f522c8ed87615a161c188b723fb20a3
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-08-12 14:21:34 +0300

    salary payment distribution request validation: restricting duplicated overlaping entry

--- customized_forcommon/common_customization/doctype/salary_payment_distribution_request/salary_payment_distribution_request.js
+++ customized_forcommon/common_customization/doctype/salary_payment_distribution_request/salary_payment_distribution_request.js
@@ -1,8 +1,18 @@
 // Copyright (c) 2026, Guba Technology and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Salary Payment Distribution Request", {
-// 	refresh(frm) {
+frappe.ui.form.on("Salary Payment Distribution Request", {
+    setup(frm) {
+        frm.set_query("employee", () => {
+            return {
+                filters: {
+                    company: frm.doc.company,
+                    status: "Active"
+                }
+            };
+        });
+    },
+    refresh(frm) {
 
-// 	},
-// });
+    },
+});
