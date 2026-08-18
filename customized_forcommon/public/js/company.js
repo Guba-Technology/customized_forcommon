576297a2bb7023cde7b8d434a5f6d5a7d2f98394
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-08-18 15:48:01 +0300

    Profit Tax Customization

--- customized_forcommon/public/js/company.js
+++ customized_forcommon/public/js/company.js
@@ -4,6 +4,19 @@ frappe.ui.form.on("Company", {
             filters: {
                 account_type: "Receivable",
                 is_group: 0,
+                company: frm.doc.name,
+            }
+        }));
+        frm.set_query("custom_profit_tax_expense_account", () => ({
+            filters: {
+                company: frm.doc.name,
+                is_group: 0,
+            }
+        }));
+        frm.set_query("custom_profit_tax_provision_account", () => ({
+            filters: {
+                company: frm.doc.name,
+                is_group: 0,
             }
         }));
     }
