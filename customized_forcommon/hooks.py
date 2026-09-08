7eb526f18b8fa42a95566e1748e822a44079e26b
Sewunet Abebaw <sewunet92@gmail.com>
2026-09-08 08:09:19 +0300

    Add payment_request_extend.js to hooks.py

--- customized_forcommon/hooks.py
+++ customized_forcommon/hooks.py
@@ -193,6 +193,7 @@ app_include_js = [
     "/assets/customized_forcommon/js/whitelabel.js",
     "/assets/customized_forcommon/js/list_sidebar_override.js",
     "/assets/customized_forcommon/js/bom_creator_extended.js",
+	"/assets/customized_forcommon/js/payment_request_extend.js",    
     "/assets/customized_forcommon/js/lite_locker.js",
     "/assets/customized_forcommon/js/bank_reconciliation_statement.js",
     "/assets/customized_forcommon/js/bank_reconciliation_reversal.js"
