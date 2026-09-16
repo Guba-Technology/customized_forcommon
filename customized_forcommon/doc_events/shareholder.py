42ee20d8b7a5bda3cacf605b873f84150fb87687
Yeshiwas Dagnaw <yeshiwasdagnaw23@gmail.com>
2026-09-16 11:48:28 +0300

    shareholder issue fixed

--- customized_forcommon/doc_events/shareholder.py
+++ customized_forcommon/doc_events/shareholder.py
@@ -1,2 +1,5 @@
 def calculate_total_farmer_members(doc, method):
-    doc.custom_total_farmer_members = doc.custom_male_farmer_members + doc.custom_female_farmer_members
+    doc.custom_total_farmer_members = (
+        (doc.custom_male_farmer_members or 0)
+        + (doc.custom_female_farmer_members or 0)
+    )
